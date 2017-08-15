e05272cb3524e6dc0602a0cde2c7486c44ecc411
Michael Allen <michael@allen.digital>
2017-08-15 11:55:45 +0100

    Consume @hmcts/look-and-feel
    Make pages in the example app look like GOV.UK pages by using
    @hmcts/look-and-feel.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,9 @@
 const BaseStep = require('./steps/BaseStep');
 const Page = require('./steps/Page');
 const Journey = require('./Journey');
-const path = require('path');
-
-const patterns = () => path.resolve(__dirname, '../templates');
 
 module.exports = {
   Page,
   BaseStep,
-  Journey,
-  patterns
+  Journey
 };
