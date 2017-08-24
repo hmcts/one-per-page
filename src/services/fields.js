f02321db00744b5459948fd0fee4796f34123891
Michael Allen <michael@allen.digital>
2017-08-24 14:19:53 +0100

    Parse fields values from the session and request body

--- src/services/fields.js
+++ src/services/fields.js
@@ -1,6 +1,9 @@
+const option = require('option');
+
 class ParsedField {
-  constructor(name, value) {
+  constructor(name, id, value) {
     this.name = name;
+    this.id = id;
     this.value = value;
   }
 }
@@ -9,8 +12,29 @@ class FieldDesriptor {
   constructor(name) {
     this.name = name;
   }
-  parse(/* req */) {
-    return new ParsedField(this.name, '');
+
+  parse(req) {
+    const id = this.makeId(req.currentStep);
+
+    const valueFromSession = option
+      .fromNullable(req.session)
+      .map(session => session[id]);
+
+    const valueFromBody = option
+      .fromNullable(req.body)
+      .map(body => body[id]);
+
+    const value = valueFromBody
+      .orElse(valueFromSession)
+      .valueOrElse('');
+    return new ParsedField(this.name, id, value);
+  }
+
+  makeId(step) {
+    if (typeof step === 'undefined' || typeof step.name === 'undefined') {
+      return this.name;
+    }
+    return `${step.name}_${this.name}`;
   }
 }
 
