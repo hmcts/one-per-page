51cc130aaf64d3bc415aaa125da481a972c8190c
Michael Allen <michael@allen.digital>
2017-08-24 14:19:53 +0100

    Parse fields values from the session and request body

--- test/services/fields.test.js
+++ test/services/fields.test.js
@@ -21,16 +21,58 @@ describe('services/fields', () => {
 
     describe('#parse', () => {
       it('returns a ParsedField', () => {
-        const foo = new FieldDesriptor('first_name', 'Michael');
+        const foo = new FieldDesriptor('first_name');
         expect(foo.parse({})).to.be.an.instanceof(ParsedField);
+      });
+
+      it('fills ParsedField.value with answer from the session', () => {
+        const req = {
+          session: { NameStep_firstName: 'Michael' },
+          currentStep: { name: 'NameStep' }
+        };
+        const firstName = new FieldDesriptor('firstName');
+        expect(firstName.parse(req)).to.have.property('value', 'Michael');
+      });
+
+      it('fills ParsedField.value with answer from request body', () => {
+        const req = {
+          body: { NameStep_firstName: 'Michael' },
+          currentStep: { name: 'NameStep' }
+        };
+        const firstName = new FieldDesriptor('firstName');
+        expect(firstName.parse(req)).to.have.property('value', 'Michael');
+      });
+
+      it('prefers answer from request body over session', () => {
+        const req = {
+          body: { NameStep_firstName: 'Michael' },
+          session: { NameStep_firstName: 'John' },
+          currentStep: { name: 'NameStep' }
+        };
+        const firstName = new FieldDesriptor('firstName');
+        expect(firstName.parse(req)).to.have.property('value', 'Michael');
+      });
+    });
+
+    describe('#makeId', () => {
+      it('returns the FieldDesriptors name if step is undefined', () => {
+        const foo = new FieldDesriptor('first_name', 'Michael');
+        expect(foo.makeId()).to.eql(foo.name);
+      });
+
+      it('returns an id based on the fields name and step', () => {
+        const foo = new FieldDesriptor('first_name', 'Michael');
+        const fakeStep = { name: 'NameStep' };
+        expect(foo.makeId(fakeStep)).to.eql(`${fakeStep.name}_${foo.name}`);
       });
     });
   });
 
   describe('ParsedField', () => {
-    it('stores name and parsed value', () => {
-      const f = new ParsedField('my name', 'my value');
-      expect(f).to.have.property('name', 'my name');
+    it('stores name, id and parsed value', () => {
+      const f = new ParsedField('my_name', 'my_id', 'my value');
+      expect(f).to.have.property('name', 'my_name');
+      expect(f).to.have.property('id', 'my_id');
       expect(f).to.have.property('value', 'my value');
     });
   });
