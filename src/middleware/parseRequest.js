3b2ad423e7635a80b6f05e4b57eff19bcec5721e
Michael Allen <michael@allen.digital>
2017-08-24 12:10:27 +0100

    Make parseRequest responsible for attaching the parsed field to req
    Didn't feel right mixing concerns and having Fields attach themselves to
    the request. The parseRequest middleware is responsible for interacting
    with the request and the field is responsible for parsing itself from
    the users answers / the session.

--- src/middleware/parseRequest.js
+++ src/middleware/parseRequest.js
@@ -2,7 +2,10 @@ const parseRequest = (req, res, next) => {
   req.fields = req.fields || {};
 
   const fieldsToParse = req.currentStep.fields || [];
-  fieldsToParse.forEach(field => field.parse(req));
+  fieldsToParse.forEach(field => {
+    const parsedField = field.parse(req);
+    req.fields[parsedField.name] = parsedField;
+  });
 
   next();
 };
