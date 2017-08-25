6e4acbe6ec0a504e89f11586e91fd0db6e682436
Michael Allen <michael@allen.digital>
2017-08-25 10:23:25 +0100

    Prove Question renders correctly by building one in example app

--- examples/test-app/steps/CreateSession.js
+++ examples/test-app/steps/CreateSession.js
@@ -8,6 +8,8 @@ class CreateSession extends Page {
   handler(req, res) {
     req.session.generate();
     req.session.username = 'Michael';
+    req.session.Name_firstName = 'Michael';
+    req.session.Name_lastName = 'Allen';
     res.redirect('/sessions');
   }
 }
