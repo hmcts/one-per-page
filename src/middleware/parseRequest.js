cb5f4e1758179cb1fca7d6d7cc05f935f2b06485
Michael Allen <michael@allen.digital>
2017-08-25 10:18:38 +0100

    Add fields to render locals so pages can access them

--- src/middleware/parseRequest.js
+++ src/middleware/parseRequest.js
@@ -1,5 +1,7 @@
 const parseRequest = (req, res, next) => {
   req.fields = req.fields || {};
+  res.locals = res.locals || {};
+  res.locals.fields = req.fields;
 
   const fieldsToParse = req.currentStep.fields || [];
   fieldsToParse.forEach(field => {
