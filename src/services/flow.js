6024b798622fb41eb6ac35f4c6b14b09536ba15c
Michael Allen <michael@allen.digital>
2017-08-30 15:36:35 +0100

    Allow goTos to be used conditionally

--- src/services/flow.js
+++ src/services/flow.js
@@ -1,3 +1,16 @@
+class Conditional {
+  constructor(redirector, condition) {
+    this.redirector = redirector;
+    this.condition = condition;
+  }
+
+  redirect(req, res) {
+    if (this.condition()) {
+      this.redirector.redirect(req, res);
+    }
+  }
+}
+
 class Redirector {
   constructor(step) {
     this.nextStep = step;
@@ -6,8 +19,12 @@ class Redirector {
   redirect(req, res) {
     res.redirect(this.nextStep.url);
   }
+
+  if(condition) {
+    return new Conditional(this, condition);
+  }
 }
 
 const goTo = step => new Redirector(step);
 
-module.exports = { goTo, Redirector };
+module.exports = { goTo, Redirector, Conditional };
