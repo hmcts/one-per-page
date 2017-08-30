97fda60379fc7baa5708feec208d1f51560ca2fa
Michael Allen <michael@allen.digital>
2017-08-30 16:46:20 +0100

    Add branching to flow logic

--- src/services/flow.js
+++ src/services/flow.js
@@ -9,6 +9,10 @@ class Conditional {
       this.redirector.redirect(req, res);
     }
   }
+
+  check() {
+    return this.condition();
+  }
 }
 
 class Redirector {
@@ -25,6 +29,40 @@ class Redirector {
   }
 }
 
-const goTo = step => new Redirector(step);
+class Branch {
+  constructor(...redirectors) {
+    const branches = Array.from(redirectors);
+    const last = branches.pop();
+    if (branches.length === 0) { // eslint-disable-line no-magic-numbers
+      throw new Error('Branch needs atleast two paths');
+    }
+    if (!branches.every(b => b instanceof Conditional)) {
+      throw new Error('All but the last path must be conditional');
+    }
+    if (last instanceof Conditional) {
+      throw new Error('The last path must not be conditional');
+    }
+    this.branches = branches;
+    this.fallback = last;
+  }
 
-module.exports = { goTo, Redirector, Conditional };
+  redirect(req, res) {
+    const findBranchThatPasses = branches => {
+      if (branches.length === 0) { // eslint-disable-line no-magic-numbers
+        return this.fallback;
+      }
+      const [current, ...rest] = branches;
+      if (current.check()) {
+        return current;
+      }
+      return findBranchThatPasses(rest);
+    };
+
+    findBranchThatPasses(this.branches).redirect(req, res);
+  }
+}
+
+const goTo = step => new Redirector(step);
+const branch = (...redirectors) => new Branch(...redirectors);
+
+module.exports = { goTo, Redirector, Conditional, branch, Branch };
