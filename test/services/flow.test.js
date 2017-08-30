649eccda2a1f19e4f2d668ea4ad04b060f1db193
Michael Allen <michael@allen.digital>
2017-08-30 15:34:14 +0100

    Refactor goTo function to produce a Redirector object

--- test/services/flow.test.js
+++ test/services/flow.test.js
@@ -1,19 +1,22 @@
 const { expect, sinon } = require('../util/chai');
-const { goTo } = require('../../src/services/flow');
+const { goTo, Redirector } = require('../../src/services/flow');
 
 describe('services/flow', () => {
   describe('#goTo', () => {
-    it('returns an object with a redirect method', () => {
+    it('returns a Redirector with a redirect method', () => {
       const fakeStep = { url: '/foo' };
-      expect(goTo(fakeStep)).to.be.an('object');
+      expect(goTo(fakeStep)).to.be.an.instanceof(Redirector);
       expect(goTo(fakeStep).redirect).to.be.a('function');
     });
+  });
+
+  describe('Redirector', () => {
 
     describe('#redirect', () => {
       it('redirects to the given step', () => {
         const fakeStep = { url: '/foo' };
         const fakeRes = { redirect: sinon.stub() };
-        goTo(fakeStep).redirect({}, fakeRes);
+        new Redirector(fakeStep).redirect({}, fakeRes);
         expect(fakeRes.redirect).calledOnce;
         expect(fakeRes.redirect).calledWith(fakeStep.url);
       });
