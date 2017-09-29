cbd1512990c6cb4cf1cd7a79b96e09ce39c50580
Michael Allen <michael@michaelallen.io>
2017-09-29 16:53:29 +0100

    Proxy over form instead of exposing it

--- src/forms/formProxyHandler.js
+++ src/forms/formProxyHandler.js
@@ -1,5 +1,14 @@
+const notProxiedFuncs = ['validate', 'store'];
+const notProxiedGetters = ['errors', 'validated', 'valid'];
+
 const formProxyHandler = {
   get(target, name) {
+    if (notProxiedFuncs.includes(name)) {
+      return target[name].bind(target);
+    }
+    if (notProxiedGetters.includes(name)) {
+      return target[name];
+    }
     const field = target.fields.find(f => f.name === name);
     return field;
   }
--- test/forms/formProxyHandler.test.js
+++ test/forms/formProxyHandler.test.js
@@ -1,20 +1,50 @@
-const { expect } = require('../util/chai');
+const { expect, sinon } = require('../util/chai');
 const { form, field } = require('../../src/forms');
 const formProxyHandler = require('../../src/forms/formProxyHandler');
 
 describe('forms/formProxyHandler', () => {
   describe('#get', () => {
-    it('proxies the name of a field to that field in the form', () => {
+    it('proxy.[field] -> form.fields[field]', () => {
       const nameField = field('name');
       const _form = form(nameField);
       const proxy = new Proxy(_form, formProxyHandler);
       expect(proxy.name.name).to.eql(nameField.name);
     });
 
-    it('returns undefined when accessing an undefined field', () => {
+    it('proxy.[undefined field] -> undefined', () => {
       const _form = form();
       const proxy = new Proxy(_form, formProxyHandler);
       expect(proxy.name).to.be.undefined;
     });
+
+    {
+      const notProxied = ['validate', 'store'];
+
+      notProxied.forEach(property => {
+        it(`proxy.${property} -> form.${property}`, () => {
+          const _form = form();
+          const proxy = new Proxy(_form, formProxyHandler);
+          const stub = sinon.stub(_form, property);
+
+          proxy[property]();
+          expect(stub).calledOnce;
+        });
+      });
+    }
+
+    {
+      const _form = form();
+      const proxy = new Proxy(_form, formProxyHandler);
+
+      it('proxy.errors -> form.errors', () => {
+        expect(proxy.errors).to.eql(_form.errors);
+      });
+      it('proxy.valid -> form.valid', () => {
+        expect(proxy.valid).to.eql(_form.valid);
+      });
+      it('proxy.validated -> form.validated', () => {
+        expect(proxy.validated).to.eql(_form.validated);
+      });
+    }
   });
 });
