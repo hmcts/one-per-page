62fa0d68539d341f6ac149bd5fd5fa507d3b1737
Michael Allen <michael@michaelallen.io>
2017-10-06 12:19:16 +0100

    Replace applyContent middleware to load a steps content in to i18Next

---
+++ src/i18n/loadStepContent.js
@@ -0,0 +1,83 @@
+const { fileExists, readJson, glob } = require('../util/fs');
+const { notDefined, defined } = require('../util/checks');
+
+const langInFilepathRegex = /^.*\.(\w{2}(?:-\w{2})?)\.json$/;
+const langRegex = /^\w{2}(?:-\w{2})?$/;
+
+const keyParseError = (key, filepath) =>
+  `top level key: ${key} in ${filepath} not a country code`;
+
+const parseI18N = (filepath, contents) => {
+  const match = filepath.match(langInFilepathRegex);
+
+  if (defined(match) && defined(match[1])) {
+    return [{ lang: match[1], translations: contents }];
+  }
+
+  return Object.keys(contents)
+    .map(key => {
+      if (defined(key.match(langRegex))) {
+        return [{ lang: key, translations: contents[key] }];
+      }
+      throw new Error(keyParseError(key, filepath));
+    })
+    .reduce((acc, arr) => [...acc, ...arr], []);
+};
+
+const applyContent = (req, res, next) => {
+  const step = req.currentStep;
+  if (notDefined(step) || notDefined(step.name) || notDefined(step.dirname)) {
+    next(new Error('req.currentStep is not a Step'));
+    return;
+  }
+  if (notDefined(step.content)) {
+    next(new Error('req.currentStep has no content proxy set up'));
+    return;
+  }
+  if (notDefined(req.i18Next)) {
+    next(new Error('i18Next not configured'));
+    return;
+  }
+
+  const addResourceBundles = filepath => contents => {
+    const bundles = parseI18N(filepath, contents);
+    bundles.forEach(({ lang, translations }) => {
+      const deep = true;
+      const overwrite = true;
+      req.i18Next.addResourceBundle(
+        lang,
+        step.name,
+        translations,
+        deep,
+        overwrite
+      );
+    });
+  };
+  const loadContents = filepaths => {
+    const loadPromises = filepaths.map(filepath =>
+      fileExists(filepath)
+        .then(readJson)
+        .then(addResourceBundles(filepath))
+    );
+    return Promise.all(loadPromises);
+  };
+
+  const promises = [
+    glob(`${step.dirname}/content.json`).then(loadContents),
+    glob(`${step.dirname}/content.@(*).json`).then(loadContents),
+    glob(`${step.dirname}/${step.name}.json`).then(loadContents),
+    glob(`${step.dirname}/${step.name}.@(*).json`).then(loadContents),
+    glob(`${step.dirname}/${step.name}.content.json`).then(loadContents),
+    glob(`${step.dirname}/${step.name}.content.@(*).json`).then(loadContents)
+  ];
+
+  Promise.all(promises).then(
+    () => {
+      req.i18Next.setDefaultNamespace(step.name);
+      next();
+    },
+    error => next(error)
+  );
+};
+
+module.exports = applyContent;
---
+++ test/i18n/loadStepContent.test.js
@@ -0,0 +1,72 @@
+const { expect } = require('../util/chai');
+const i18Next = require('i18next');
+const loadStepContent = require('../../src/i18n/loadStepContent');
+const { proxyHandler } = require('../../src/i18n/contentProxy');
+const path = require('path');
+const { middlewareTest } = require('../util/middlewareTest');
+
+describe('i18n/loadStepContent', () => {
+  it('presents an express middleware', () => {
+    expect(loadStepContent).to.be.a('function');
+    expect(loadStepContent.toString()).to.match(/req,res,next/);
+  });
+
+  it('expects req.currentStep to be a step', () => {
+    const shouldFail = middlewareTest({ currentStep: {} })
+      .use(loadStepContent)
+      .run();
+    return expect(shouldFail).rejectedWith('req.currentStep is not a Step');
+  });
+
+  it('expects req.currentStep to have a content proxy set', () => {
+    const currentStep = { dirname: '', name: '' };
+    const shouldFail = middlewareTest({ currentStep })
+      .use(loadStepContent)
+      .run();
+    return expect(shouldFail).rejectedWith(/no content proxy/);
+  });
+
+  it('expects req.i18Next to be defined', () => {
+    const currentStep = {
+      dirname: '',
+      name: '',
+      content: new Proxy({}, proxyHandler)
+    };
+    const shouldFail = middlewareTest({ currentStep })
+      .use(loadStepContent)
+      .run();
+    return expect(shouldFail).rejectedWith(/i18Next not configured/);
+  });
+
+  const tests = [
+    'content.json',
+    'content.en.json',
+    'StepName.json',
+    'StepName.en.json',
+    'StepName.content.json',
+    'StepName.content.en.json'
+  ];
+
+  const testRoot = path.resolve(__dirname, './fixtures/loadStepContent');
+  const i18N = i18Next.init();
+  i18N.changeLanguage('en');
+  const request = {
+    i18Next: i18N,
+    currentStep: {
+      dirname: testRoot,
+      name: 'StepName',
+      content: new Proxy(i18N, proxyHandler)
+    }
+  };
+  const loadContentTest = middlewareTest(request)
+    .use(loadStepContent)
+    .run();
+
+  tests.forEach(filename => {
+    it(`loads content from ${filename}`, () => {
+      const key = filename.replace(/\./g, '_dot_');
+      return loadContentTest
+        .then(req => expect(req.i18Next.t(key)).to.eql('Expected Value'));
+    });
+  });
+});
