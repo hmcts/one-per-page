22c560262f7bdfa68e7073a0d98d74bec14a1b5c
Michael Allen <michael@michaelallen.io>
2017-10-09 10:01:52 +0100

    Remove Page.content so pages now resolve from content files

--- src/steps/Page.js
+++ src/steps/Page.js
@@ -1,33 +1,10 @@
 const BaseStep = require('./BaseStep');
 const addLocals = require('../middleware/addLocals');
 const { METHOD_NOT_ALLOWED } = require('http-status-codes');
-const i18next = require('i18next');
-const walkMap = require('../util/walkMap');
 const loadStepContent = require('../i18n/loadStepContent');
 const resolveTemplate = require('../middleware/resolveTemplate');
 
 class Page extends BaseStep {
-  constructor() {
-    super();
-
-    if (this.i18NextContent) {
-      this.i18next = i18next.createInstance();
-      this.i18next.init({
-        lng: 'en',
-        resources: this.i18NextContent
-      });
-    }
-  }
-
-  get content() {
-    if (!this.i18next) {
-      return {};
-    }
-
-    return walkMap(this.i18NextContent.en.translation, path =>
-      this.i18next.t(path, this.locals));
-  }
-
   get middleware() {
     return [resolveTemplate, addLocals, loadStepContent];
   }
