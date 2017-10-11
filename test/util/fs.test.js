1638cfb26a86b36633f30eb8855c256f17d018c9
Michael Allen <michael@michaelallen.io>
2017-10-11 16:49:47 +0100

    Test fs#glob

--- test/util/fs.test.js
+++ test/util/fs.test.js
@@ -1,6 +1,6 @@
 const { expect } = require('./chai');
 const path = require('path');
-const { fileExists } = require('../../src/util/fs');
+const { fileExists, glob } = require('../../src/util/fs');
 
 describe('util/fs', () => {
   describe('#fileExists', () => {
@@ -17,6 +17,34 @@ describe('util/fs', () => {
     it('rejects if the path isnt a file', () => {
       const file = path.resolve(__dirname, '.');
       return expect(fileExists(file)).to.eventually.be.rejected;
+    });
+  });
+
+  describe('#glob', () => {
+    const fixturesDir = path.resolve(__dirname, 'fixtures/glob/');
+
+    it('resolves with a filename', () => {
+      const pattern = path.resolve(fixturesDir, 'file1.txt');
+      return expect(glob(pattern)).to.eventually.eql([pattern]);
+    });
+
+    it('resolves with all filenames that match a pattern', () => {
+      const pattern = path.resolve(fixturesDir, 'file*.txt');
+      const expected = [
+        path.resolve(fixturesDir, 'file1.txt'),
+        path.resolve(fixturesDir, 'file2.txt'),
+        path.resolve(fixturesDir, 'file3.txt')
+      ];
+      return expect(glob(pattern)).to.eventually.eql(expected);
+    });
+
+    it('resolves with an empty array if nothing matched', () => {
+      const pattern = path.resolve(fixturesDir, 'nonexistant.txt');
+      return expect(glob(pattern)).to.eventually.eql([]);
+    });
+
+    it('rejects if no path provided', () => {
+      return expect(glob()).rejectedWith(/pattern string required/);
     });
   });
 });
