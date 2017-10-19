94697f9081c2ebce147aa70cd99c30dee9065b7d
Michael Allen <michael@michaelallen.io>
2017-10-19 13:16:24 +0100

    Allow steps to define functions that are scoped local

--- src/steps/Page.js
+++ src/steps/Page.js
@@ -6,6 +6,17 @@ const resolveTemplate = require('../middleware/resolveTemplate');
 const { i18NextInstance } = require('../i18n/i18Next');
 const { contentProxy } = require('../i18n/contentProxy');
 
+const notLocals = [
+  '_router',
+  'journey',
+  'constructor',
+  'form',
+  'next',
+  'locals',
+  'middleware',
+  'handler'
+];
+
 class Page extends BaseStep {
   constructor() {
     super();
@@ -17,7 +28,19 @@ class Page extends BaseStep {
   }
 
   get locals() {
-    return this.res.locals;
+    const myKeys = [
+      ...Reflect.ownKeys(this),
+      ...Reflect.ownKeys(Object.getPrototypeOf(this))
+    ];
+
+    const classLocals = myKeys
+      .filter(key => !(notLocals.includes(key)))
+      .reduce((obj, key) => {
+        obj[key] = this[key];
+        return obj;
+      }, {});
+
+    return Object.assign({}, classLocals, this.res.locals);
   }
 
   handler(req, res) {
