f562aff419ce94a3c947d406bee937aaa78ea1ee
Michael Allen <michael@michaelallen.io>
2017-11-16 11:08:00 +0000

    Walk the tree to build a list of steps the user has visited

---
+++ src/flow/walkTree.js
@@ -0,0 +1,28 @@
+const Question = require('../steps/Question');
+
+const walkTree = (start, steps) => {
+  let current = start;
+  let iterations = 0;
+  const results = [];
+  const numberOfSteps = Object.keys(steps).length;
+  while (iterations <= numberOfSteps) {
+    results.push(current);
+
+    if (current instanceof Question) {
+      current.retrieve().validate();
+
+      if (!current.valid) {
+        return results;
+      }
+    }
+    if (typeof current.next === 'undefined') {
+      return results;
+    }
+    const next = current.next().step.name;
+    current = steps[next];
+    iterations += 1;
+  }
+  throw new Error('possible infinite loop encountered');
+};
+
+module.exports = walkTree;
