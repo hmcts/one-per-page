dd342c75edc416d3fcb296689badb7889b3f5045
Michael Allen <michael@michaelallen.io>
2017-11-17 09:24:09 +0000

    Add boolField for parsing yes/nos to boolean

--- src/forms/index.js
+++ src/forms/index.js
@@ -13,4 +13,14 @@ const nonEmptyTextField = name => field(name, parsers.nonEmptyTextParser);
 nonEmptyTextField.ref = (step, name) =>
   ref(step, name, parsers.nonEmptyTextParser);
 
-module.exports = { form, field, arrayField, textField, nonEmptyTextField };
+const boolField = name => field(name, parsers.boolParser);
+boolField.ref = (step, name) => ref(step, name, parsers.boolParser);
+
+module.exports = {
+  form,
+  field,
+  arrayField,
+  textField,
+  nonEmptyTextField,
+  boolField
+};
