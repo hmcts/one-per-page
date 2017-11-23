c5af443c1741ad6024e64cfdd178bc807e6d7789
Michael Allen <michael@michaelallen.io>
2017-11-23 10:33:31 +0000

    Switch to using RequestBoundJourneys walk tree

--- src/flow/index.js
+++ src/flow/index.js
@@ -1,10 +1,9 @@
 const Redirector = require('./redirector');
 const Branch = require('./branch');
 const Stop = require('./stop');
-const walkTree = require('./walkTree');
 
 const goTo = step => new Redirector(step);
 const branch = (...redirectors) => new Branch(...redirectors);
 const stop = step => new Stop(step);
 
-module.exports = { goTo, branch, stop, walkTree };
+module.exports = { goTo, branch, stop };
