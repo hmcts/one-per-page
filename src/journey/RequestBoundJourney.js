f613ed88b0d8a1a29c010a533f0745c9de7f63ee
Michael Allen <michael@michaelallen.io>
2017-11-23 10:28:44 +0000

    Copy walkTree functionality in to RequestBoundJourney

--- src/journey/RequestBoundJourney.js
+++ src/journey/RequestBoundJourney.js
@@ -1,4 +1,6 @@
 const { defined } = require('../util/checks');
+const Question = require('../steps/Question');
+const Stop = require('../flow/stop');
 
 const getName = stepOrName => {
   if (typeof stepOrName === 'string') {
@@ -30,6 +32,41 @@ class RequestBoundJourney {
       return this.instances[name];
     }
     throw new Error(`${name} not registered`);
+  }
+
+  get entryPoint() {
+    if (defined(this.req.session.entryPoint)) {
+      return this.req.session.entryPoint;
+    }
+    throw new Error('No entryPoint defined in session');
+  }
+
+  walkTree(from = this.entryPoint) {
+    let current = this.instance(from);
+    let iterations = 0;
+    const results = [];
+    const numberOfSteps = Object.keys(this.steps).length;
+    while (iterations <= numberOfSteps) {
+      if (current instanceof Question) {
+        results.push(current);
+        current.retrieve().validate();
+
+        if (!current.valid) {
+          return results;
+        }
+      }
+      if (typeof current.next === 'undefined') {
+        return results;
+      }
+      if (current.next() instanceof Stop) {
+        return results;
+      }
+      const nextStep = current.next().step;
+      current = this.instance(nextStep);
+
+      iterations += 1;
+    }
+    throw new Error('possible infinite loop encountered');
   }
 }
 
--- test/journey/RequestBoundJourney.test.js
+++ test/journey/RequestBoundJourney.test.js
@@ -1,6 +1,12 @@
 const { expect } = require('../util/chai');
 const RequestBoundJourney = require('../../src/journey/RequestBoundJourney');
 const BaseStep = require('../../src/steps/BaseStep');
+const EntryPoint = require('../../src/steps/EntryPoint');
+const Question = require('../../src/steps/Question');
+const CheckYourAnswers = require('../../src/steps/check-your-answers/CheckYourAnswers'); // eslint-disable-line max-len
+const { branch, goTo } = require('../../src/flow');
+const { textField, form } = require('../../src/forms');
+const Joi = require('joi');
 
 describe('journey/RequestBoundJourney', () => {
   it('sets itself to req.journey', () => {
@@ -44,6 +50,184 @@ describe('journey/RequestBoundJourney', () => {
       const arbitraryObj = () => journey.instance({});
 
       expect(arbitraryObj).to.throw(/is not a step/);
+    });
+  });
+
+  describe('#walkTree', () => {
+    describe('Simple complete journey', () => {
+      class Entry extends EntryPoint {
+        next() {
+          return goTo(this.journey.steps.Name);
+        }
+      }
+      class Name extends Question {
+        get form() {
+          return form(textField('firstName'), textField('lastName'));
+        }
+        next() {
+          return goTo(this.journey.steps.CheckAnswers);
+        }
+      }
+      class CheckAnswers extends CheckYourAnswers {}
+      const steps = { Entry, Name, CheckAnswers };
+      const session = {
+        entryPoint: Entry.name,
+        Name: { firstName: 'Michael', lastName: 'Allen' },
+        CheckAnswers: { statementOfTruth: true }
+      };
+      const res = {};
+      const req = { session };
+
+      it('returns instances of the questions in the order of the flow', () => {
+        const journey = new RequestBoundJourney(req, res, steps, {});
+        const names = journey.walkTree().map(s => s.name);
+        expect(names).to.eql([Name.name, CheckAnswers.name]);
+      });
+
+      it('retrieves and validates any questions', () => {
+        const journey = new RequestBoundJourney(req, res, steps, {});
+        const valids = journey.walkTree().map(step => step.fields.valid);
+        expect(valids).to.eql([true, true]);
+      });
+    });
+
+    describe('Simple incomplete journey', () => {
+      class Entry extends EntryPoint {
+        next() {
+          return goTo(this.journey.steps.Name);
+        }
+      }
+      class Name extends Question {
+        get form() {
+          return form(textField('notPresent').joi(Joi.string().required()));
+        }
+        next() {
+          return goTo(this.journey.steps.CheckAnswers);
+        }
+      }
+      class CheckAnswers extends CheckYourAnswers {}
+      const steps = { Entry, Name, CheckAnswers };
+      const session = {
+        entryPoint: Entry.name,
+        Name: {}
+      };
+      const req = { session };
+      const res = {};
+
+      it('stops on the first incomplete step', () => {
+        const journey = new RequestBoundJourney(req, res, steps, {});
+        const names = journey.walkTree().map(s => s.name);
+        expect(names).to.eql([Name.name]);
+      });
+    });
+
+    describe('Branching journey', () => {
+      class Entry extends EntryPoint {
+        next() {
+          return goTo(this.journey.steps.Branch);
+        }
+      }
+      class Branch extends Question {
+        get form() {
+          return form(textField('branchControl'));
+        }
+        next() {
+          const isA = this.fields.branchControl.value === 'A';
+          const isB = this.fields.branchControl.value === 'B';
+          return branch(
+            goTo(this.journey.steps.A).if(isA),
+            goTo(this.journey.steps.B).if(isB),
+            goTo(this.journey.steps.CheckAnswers)
+          );
+        }
+      }
+      class B extends Question {
+        next() {
+          return goTo(this.journey.steps.CheckAnswers);
+        }
+      }
+      class A extends Question {
+        next() {
+          return goTo(this.journey.steps.CheckAnswers);
+        }
+      }
+      class CheckAnswers extends CheckYourAnswers {}
+      const steps = { Entry, A, B, Branch, CheckAnswers };
+      const session = {
+        entryPoint: Entry.name,
+        Branch: { branchControl: 'A' }
+      };
+      const req = { session };
+      const res = {};
+
+      it('skips steps that are not accessed by the flow', () => {
+        const journey = new RequestBoundJourney(req, res, steps, {});
+        const names = journey.walkTree().map(s => s.name);
+        expect(names).to.eql([Branch.name, A.name, CheckAnswers.name]);
+      });
+    });
+
+    describe('loop protection', () => {
+      {
+        class Entry extends EntryPoint {
+          next() {
+            return goTo(this.journey.steps.A);
+          }
+        }
+        class A extends Question {
+          next() {
+            return goTo(this.journey.steps.B);
+          }
+        }
+        class B extends Question {
+          next() {
+            return goTo(this.journey.steps.A);
+          }
+        }
+        class CheckAnswers extends CheckYourAnswers {}
+        const steps = { Entry, A, B, CheckAnswers };
+        const req = { session: { entryPoint: Entry.name } };
+        const res = {};
+
+        it('protects against infinite loops', () => {
+          const journey = new RequestBoundJourney(req, res, steps, {});
+          const tightLoop = () => journey.walkTree();
+          expect(tightLoop).to.throw(/possible infinite loop/);
+        });
+      }
+
+      {
+        class Entry extends EntryPoint {
+          next() {
+            return goTo(this.journey.steps.A);
+          }
+        }
+        class A extends Question {
+          next() {
+            return goTo(this.journey.steps.B);
+          }
+        }
+        class B extends Question {
+          next() {
+            return goTo(this.journey.steps.C);
+          }
+        }
+        class C extends Question {
+          next() {
+            return goTo(this.journey.steps.A);
+          }
+        }
+        class CheckAnswers extends CheckYourAnswers {}
+        const steps = { Entry, A, B, C, CheckAnswers };
+        const req = { session: { entryPoint: Entry.name } };
+        const res = {};
+
+        it('protects against infinite loops', () => {
+          const journey = new RequestBoundJourney(req, res, steps, {});
+          const looseLoop = () => journey.walkTree();
+          expect(looseLoop).to.throw(/possible infinite loop/);
+        });
+      }
     });
   });
 });
