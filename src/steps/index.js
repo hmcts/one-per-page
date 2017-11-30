5a64cf131179be5a286635e0845045183894be7c
Michael Allen <michael@michaelallen.io>
2017-11-30 14:54:29 +0000

    Expose steps and flow modules

---
+++ src/steps/index.js
@@ -0,0 +1,17 @@
+const BaseStep = require('./BaseStep');
+const Question = require('./Question');
+const EntryPoint = require('./EntryPoint');
+const ExitPoint = require('./ExitPoint');
+const Page = require('./Page');
+const Redirect = require('./Redirect');
+const Action = require('./Action');
+
+module.exports = {
+  BaseStep,
+  Question,
+  EntryPoint,
+  ExitPoint,
+  Page,
+  Redirect,
+  Action
+};
