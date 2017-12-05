f73ca4bb2b46168ada46697cb21426e25facc9c5
Michael Allen <michael@michaelallen.io>
2017-12-05 16:40:33 +0000

    Add action flow control that performs some action before redirecting

---
+++ src/flow/action.js
@@ -0,0 +1,50 @@
+const { notDefined } = require('../util/checks');
+const logging = require('@log4js-node/log4js-api');
+
+class Action {
+  constructor(action, nextFlow, errorFlow) {
+    this.action = action;
+    this.nextFlow = nextFlow;
+    this.errorFlow = errorFlow;
+  }
+
+  redirect(req, res) {
+    const promise = this.performAction(req, res);
+
+    return promise
+      .then(() => {
+        if (notDefined(this.nextFlow)) {
+          logging.getLogger(this.name).error('No flow chained to action');
+          return;
+        }
+        this.nextFlow.redirect(req, res);
+      })
+      .catch(() => {
+        if (notDefined(this.errorFlow)) {
+          logging.getLogger(this.name).error('No error flow chained to action');
+          return;
+        }
+        this.errorFlow.redirect(req, res);
+      });
+  }
+
+  performAction(req, res) {
+    try {
+      return Promise.resolve(this.action(req, res));
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+
+  then(nextFlow) {
+    this.nextFlow = nextFlow;
+    return this;
+  }
+
+  onFailure(errorFlow) {
+    this.errorFlow = errorFlow;
+    return this;
+  }
+}
+
+module.exports = Action;
---
+++ test/flow/action.test.js
@@ -0,0 +1,103 @@
+const Action = require('../../src/flow/action');
+const { expect, sinon } = require('../util/chai');
+const { Defer } = require('../../src/util/promises');
+const logger = require('@log4js-node/log4js-api');
+
+describe('flow/action', () => {
+  const actionStub = sinon.stub();
+  const nextStub = {
+    redirect: sinon.stub(),
+    step: sinon.stub()
+  };
+  const req = {};
+  const res = { send: sinon.stub() };
+
+  beforeEach(() => {
+    sinon.stub(logger, 'getLogger');
+    actionStub.reset();
+    nextStub.redirect.reset();
+    nextStub.step.reset();
+    res.send.reset();
+  });
+
+  afterEach(() => {
+    logger.getLogger.restore();
+  });
+
+  describe('#then', () => {
+    it('sets the actions nextFlow to the given flow', () => {
+      const action = new Action(actionStub);
+      expect(action.nextFlow).undefined;
+
+      action.then(nextStub);
+      expect(action.nextFlow).eql(nextStub);
+    });
+  });
+
+  describe('#onFailure', () => {
+    it('sets the actions errorFlow to the given flow', () => {
+      const action = new Action(actionStub);
+      expect(action.errorFlow).undefined;
+
+      action.onFailure(nextStub);
+      expect(action.errorFlow).eql(nextStub);
+    });
+  });
+
+  describe('#redirect', () => {
+    it('executes the action given to it', () => {
+      const action = new Action(actionStub).then(nextStub);
+      action.redirect(req, res);
+
+      expect(actionStub).calledOnce;
+      expect(actionStub).calledWith(req, res);
+    });
+
+    it('calls the next redirector in it\'s chain', () => {
+      const action = new Action(actionStub).then(nextStub);
+      const promise = action.redirect(req, res);
+      return promise.then(() => expect(nextStub.redirect).calledOnce);
+    });
+
+    it('waits for if the action returns a promise', () => {
+      const defer = new Defer();
+      actionStub.returns(defer);
+
+      const action = new Action(actionStub).then(nextStub);
+      const promise = action.redirect(req, res);
+      expect(nextStub.redirect).not.called;
+
+      defer.resolve();
+      return promise.then(() => expect(nextStub.redirect).calledOnce);
+    });
+
+    it('logs if no nextFlow is given', () => {
+      const errorLogger = sinon.stub();
+      logger.getLogger.returns({ error: errorLogger });
+      return new Action(actionStub)
+        .redirect(req, res)
+        .then(() =>
+          expect(errorLogger).calledWith('No flow chained to action')
+        );
+    });
+
+    it('calls the error redirector if the action throws', () => {
+      const action = new Action(actionStub)
+        .onFailure(nextStub);
+      actionStub.throws(new Error('I failed'));
+
+      return action
+        .redirect(req, res)
+        .then(() => expect(nextStub.redirect).calledOnce);
+    });
+    it('calls the error redirector if the promise fails', () => {
+      const action = new Action(actionStub)
+        .onFailure(nextStub);
+      actionStub.returns(Promise.reject(new Error('I failed')));
+
+      return action
+        .redirect(req, res)
+        .then(() => expect(nextStub.redirect).calledOnce);
+    });
+  });
+});
