f5088642eb64f0c865a95978a05971ddd78bfa3e
Michael Allen <michael@michaelallen.io>
2017-12-05 16:40:33 +0000

    Add action flow control that performs some action before redirecting

--- src/flow/index.js
+++ src/flow/index.js
@@ -3,6 +3,7 @@ const Branch = require('./branch');
 const Stop = require('./stop');
 const journey = require('./journey');
 const RequestBoundJourney = require('./RequestBoundJourney');
+const Action = require('./action');
 const {
   ifCompleteThenContinue,
   continueToNext,
@@ -13,11 +14,13 @@ const {
 const goTo = step => new Redirector(step);
 const branch = (...redirectors) => new Branch(...redirectors);
 const stop = step => new Stop(step);
+const action = redirector => new Action(redirector);
 
 module.exports = {
   goTo,
   branch,
   stop,
+  action,
   journey,
   RequestBoundJourney,
   ifCompleteThenContinue,
