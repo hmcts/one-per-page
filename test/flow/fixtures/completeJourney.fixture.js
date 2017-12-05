8a2bf3dbec0f29844f9f48ab5b60c17cfb222bfd
Michael Allen <michael@michaelallen.io>
2017-12-05 10:12:14 +0000

    Clean up tests for the RequestBoundJourney

---
+++ test/flow/fixtures/completeJourney.fixture.js
@@ -0,0 +1,32 @@
+const EntryPoint = require('../../../src/steps/EntryPoint');
+const Question = require('../../../src/steps/Question');
+const CheckYourAnswers = require('../../../src/steps/check-your-answers/CheckYourAnswers'); // eslint-disable-line max-len
+const { goTo, RequestBoundJourney } = require('../../../src/flow');
+const { textField, form } = require('../../../src/forms');
+
+class Entry extends EntryPoint {
+  next() {
+    return goTo(this.journey.steps.Name);
+  }
+}
+class Name extends Question {
+  get form() {
+    return form(textField('firstName'), textField('lastName'));
+  }
+  next() {
+    return goTo(this.journey.steps.CheckAnswers);
+  }
+}
+class CheckAnswers extends CheckYourAnswers {}
+const steps = { Entry, Name, CheckAnswers };
+const session = {
+  entryPoint: Entry.name,
+  Name: { firstName: 'Michael', lastName: 'Allen' },
+  CheckAnswers: { statementOfTruth: true }
+};
+const res = {};
+const req = { session };
+
+const journey = () => new RequestBoundJourney(req, res, steps, {});
+
+module.exports = { journey, Entry, Name, CheckAnswers };
---
+++ test/flow/fixtures/longLoopingJourney.fixture.js
@@ -0,0 +1,33 @@
+const EntryPoint = require('../../../src/steps/EntryPoint');
+const Question = require('../../../src/steps/Question');
+const { goTo, RequestBoundJourney } = require('../../../src/flow');
+const CheckYourAnswers = require('../../../src/steps/check-your-answers/CheckYourAnswers'); // eslint-disable-line max-len
+
+class Entry extends EntryPoint {
+  next() {
+    return goTo(this.journey.steps.A);
+  }
+}
+class A extends Question {
+  next() {
+    return goTo(this.journey.steps.B);
+  }
+}
+class B extends Question {
+  next() {
+    return goTo(this.journey.steps.C);
+  }
+}
+class C extends Question {
+  next() {
+    return goTo(this.journey.steps.A);
+  }
+}
+class CheckAnswers extends CheckYourAnswers {}
+const steps = { Entry, A, B, C, CheckAnswers };
+const req = { session: { entryPoint: Entry.name } };
+const res = {};
+
+const journey = new RequestBoundJourney(req, res, steps, {});
+
+module.exports = journey;
