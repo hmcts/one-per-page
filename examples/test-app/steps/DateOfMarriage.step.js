cbec7184ba9c2cfa0f62e8c3ad9ed7c394ea857a
Michael Allen <michael@michaelallen.io>
2017-12-11 15:03:08 +0000

    Use dateField to ask for the users date of marriage

---
+++ examples/test-app/steps/DateOfMarriage.step.js
@@ -0,0 +1,50 @@
+const { Question } = require('@hmcts/one-per-page/steps');
+const { form, dateField } = require('@hmcts/one-per-page/forms');
+const { branch, goTo } = require('@hmcts/one-per-page/flow');
+const { answer } = require('@hmcts/one-per-page/checkYourAnswers');
+const moment = require('moment');
+
+class DateOfMarriage extends Question {
+  get form() {
+    return form(
+      dateField(
+        'date',
+        {
+          allRequired: this.content.date.allRequired,
+          dayRequired: this.content.date.dayRequired,
+          monthRequired: this.content.date.monthRequired,
+          yearRequired: this.content.date.yearRequired
+        }
+      ).mapValue(
+        ({ day, month, year }) => moment({ day, month, year })
+      )
+    );
+  }
+
+  get today() {
+    return moment().format('D M YYYY');
+  }
+
+  next() {
+    const oneYearAgo = moment().subtract(1, 'year');
+    const marriedLessThan1Year = this.fields.date.value.isAfter(oneYearAgo);
+
+    return branch(
+      goTo(this.journey.steps.ExitDate).if(marriedLessThan1Year),
+      goTo(this.journey.steps.Country)
+    );
+  }
+
+  answers() {
+    return answer(this, {
+      question: this.content.date.question,
+      answer: this.fields.date.value.format('DD/MM/YYYY')
+    });
+  }
+
+  values() {
+    return { marriage: { date: this.fields.date.value.format('YYYY-MM-DD') } };
+  }
+}
+
+module.exports = DateOfMarriage;
---
+++ examples/test-app/steps/exits/ExitDate.step.js
@@ -0,0 +1,5 @@
+const { ExitPoint } = require('@hmcts/one-per-page/steps');
+
+class ExitDate extends ExitPoint {}
+
+module.exports = ExitDate;
