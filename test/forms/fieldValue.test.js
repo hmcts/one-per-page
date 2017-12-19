8f162a32eaeb14b65093900e687d6c5f5a3aa42f
Michael Allen <michael@michaelallen.io>
2017-12-19 18:46:35 +0000

    Provide mappedErrors needed for summarising the errors

--- test/forms/fieldValue.test.js
+++ test/forms/fieldValue.test.js
@@ -1,5 +1,6 @@
 const { expect, sinon } = require('../util/chai');
 const { FieldValue } = require('../../src/forms/fieldValue');
+const FieldError = require('../../src/forms/fieldError');
 
 describe('forms/fieldValue', () => {
   describe('FieldValue', () => {
@@ -44,6 +45,44 @@ describe('forms/fieldValue', () => {
         f.validate();
         expect(willFail).calledWith(f);
         expect(willPass).not.called;
+      });
+    });
+
+    describe('#errors', () => {
+      it('returns [] if the field has not been validated', () => {
+        const f = new FieldValue({ name: 'name', value: 'value' });
+        expect(f.errors).to.eql([]);
+      });
+
+      it('returns any errors returned by validations', () => {
+        const willFail = sinon.stub().returns('An error');
+        const willPass = sinon.stub();
+        const f = new FieldValue({
+          name: 'name',
+          value: 'value',
+          validations: [willFail, willPass]
+        });
+        f.validate();
+        expect(f.errors).to.eql(['An error']);
+      });
+    });
+
+    describe('#mappedErrors', () => {
+      it('returns [] if the field has not been validated', () => {
+        const f = new FieldValue({ name: 'name', value: 'value' });
+        expect(f.mappedErrors).to.eql([]);
+      });
+
+      it('returns a FieldError for each error returned by validations', () => {
+        const willFail = sinon.stub().returns('An error');
+        const willPass = sinon.stub();
+        const f = new FieldValue({
+          name: 'name',
+          value: 'value',
+          validations: [willFail, willPass]
+        });
+        f.validate();
+        expect(f.mappedErrors).to.eql([new FieldError(f, 'An error')]);
       });
     });
   });
