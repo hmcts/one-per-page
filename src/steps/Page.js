9a63595f7238933c3d999754a5bab3f0d5b2f813
endamccormack <endamccormack0@gmail.com>
2017-12-20 09:56:20 +0000

    replaced manual strings with i18n files

--- src/steps/Page.js
+++ src/steps/Page.js
@@ -1,7 +1,7 @@
 const BaseStep = require('./BaseStep');
 const addLocals = require('../middleware/addLocals');
 const { METHOD_NOT_ALLOWED } = require('http-status-codes');
-const loadStepContent = require('../i18n/loadStepContent');
+const { loadStepContent } = require('../i18n/loadStepContent');
 const resolveTemplate = require('../middleware/resolveTemplate');
 const { i18NextInstance } = require('../i18n/i18Next');
 const { contentProxy } = require('../i18n/contentProxy');
