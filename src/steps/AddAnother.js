69f0c243514e45e1fdf402db392f307ad0ea6ac3
Michael Allen <michael@michaelallen.io>
2017-12-22 16:12:08 +0000

    Add a step type that allows you to capture mutliple of a complex field

---
+++ src/steps/AddAnother.js
@@ -0,0 +1,193 @@
+const Question = require('./Question');
+const { fieldDescriptor } = require('../forms/fieldDescriptor');
+const { defined } = require('../util/checks');
+const { flattenObject } = require('../util/ops');
+const { TransformFieldValue } = require('../forms/fieldValue');
+const { filledForm } = require('../forms/filledForm');
+const { form, list } = require('../forms');
+const option = require('option');
+const { METHOD_NOT_ALLOWED } = require('http-status-codes');
+
+const addAnother = (collectionKey, index, field) => fieldDescriptor({
+  parser(name, body, req) {
+    const fieldValue = field.parse(name, body, req);
+
+    return TransformFieldValue.from({
+      transformation: t => t,
+      wrapped: fieldValue
+    }, this);
+  },
+  deserializer(name, values) {
+    const arr = option
+      .fromNullable(values[collectionKey])
+      .valueOrElse([]);
+    const extracted = arr.length > index ? { [name]: arr[index] } : {};
+    const fieldValue = field.deserialize(name, extracted);
+
+    return TransformFieldValue.from({
+      transformation: t => t,
+      wrapped: fieldValue
+    }, this);
+  },
+  serializer(fieldValue, values) {
+    const listOfItems = list(field)
+      .deserialize(collectionKey, values)
+      .serializedValues(values) || [];
+
+    const newItem = field.serializer(fieldValue, values);
+
+    if (defined(newItem[fieldValue.name])) {
+      if (listOfItems.length > index) {
+        listOfItems[index] = newItem[fieldValue.name];
+      } else {
+        listOfItems.push(newItem[fieldValue.name]);
+      }
+    }
+    return { [collectionKey]: listOfItems };
+  }
+});
+
+
+class AddAnother extends Question {
+  get form() {
+    if (this.isListMode || this.isDeleteMode) {
+      return form({ items: this.validateList(list(this.field)) });
+    } else if (this.isEditMode) {
+      return form({ item: addAnother('items', this.index, this.field) });
+    }
+    return form();
+  }
+
+  deleteUrl(index) {
+    return `${this.path}/item-${index}/delete`;
+  }
+
+  editUrl(index) {
+    return `${this.path}/item-${index}`;
+  }
+
+  get addAnotherUrl() {
+    if (this.isListMode) {
+      return this.editUrl(this.fields.items.value.length);
+    }
+    return this.path;
+  }
+
+  get mode() {
+    if (defined(this.req.params)) {
+      if (defined(this.req.params['0'])) {
+        if (defined(this.req.params['1'])) {
+          return 'delete';
+        }
+        return 'edit';
+      }
+    }
+    return 'list-items';
+  }
+  get isListMode() {
+    return this.mode === 'list-items';
+  }
+  get isEditMode() {
+    return this.mode === 'edit';
+  }
+  get isDeleteMode() {
+    return this.mode === 'delete';
+  }
+
+  get postUrl() {
+    if (this.isEditMode) {
+      return this.editUrl(this.index);
+    }
+    return super.postUrl;
+  }
+
+  get index() {
+    if (defined(this.req.params) && defined(this.req.params['0'])) {
+      return parseInt(this.req.params['0']);
+    }
+    return -1;
+  }
+
+  handler(req, res) {
+    if (this.isListMode) {
+      this.listModeHandler(req, res);
+    } else if (this.isEditMode) {
+      this.editModeHandler(req, res);
+    } else if (this.isDeleteMode) {
+      this.deleteModeHandler(req, res);
+    } else {
+      throw new Error(`${this.mode} not recognised`);
+    }
+  }
+
+  renderPage() {
+    this.retrieve();
+    if (this.fields.filled) {
+      this.validate();
+    }
+    this.res.render(this.template, this.locals);
+  }
+
+  listModeHandler(req, res) {
+    if (req.method === 'GET') {
+      this.renderPage();
+    } else if (req.method === 'POST') {
+      this.retrieve();
+      this.validate();
+
+      if (this.valid) {
+        this.next().redirect(req, res);
+      } else {
+        res.render(this.template, this.locals);
+      }
+    } else {
+      res.sendStatus(METHOD_NOT_ALLOWED);
+    }
+  }
+
+  editModeHandler(req, res) {
+    if (req.method === 'GET') {
+      this.renderPage();
+    } else if (req.method === 'POST') {
+      this.parse();
+      this.validate();
+
+      if (this.valid) {
+        this.store();
+        res.redirect(this.path);
+      } else {
+        res.render(this.template, this.locals);
+      }
+    } else {
+      res.redirect(this.path);
+    }
+  }
+
+  deleteModeHandler(req, res) {
+    if (req.method === 'GET') {
+      this.retrieve();
+      const fieldsAsArray = Object.values(this.fields.items.fields);
+
+      if (fieldsAsArray.length > this.index) {
+        fieldsAsArray.splice(this.index, 1);
+        const newFields = fieldsAsArray
+          .map((field, i) => {
+            return { [i]: field };
+          })
+          .reduce(flattenObject, {});
+        const items = this.fields.items.clone({ fields: newFields });
+
+        this.fields = filledForm({ items });
+        this.store();
+      }
+    }
+    res.redirect(this.path);
+  }
+
+  static get pathToBind() {
+    // return `${super.path}/foo(\\d)/`;
+    return `${super.path}(?:/item-(\\d{1,})/?(?:(delete)/?)?)?`;
+  }
+}
+
+module.exports = AddAnother;
