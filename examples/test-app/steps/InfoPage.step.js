ceaaa7510d5d639fc25d73446dd1cd93c60c0215
Michael Allen <michael@michaelallen.io>
2018-03-06 18:22:56 +0000

    Demo using the interstitial step

---
+++ examples/test-app/steps/InfoPage.step.js
@@ -0,0 +1,10 @@
+const { Interstitial } = require('@hmcts/one-per-page/steps');
+const { goTo } = require('@hmcts/one-per-page/flow');
+
+class InfoPage extends Interstitial {
+  next() {
+    return goTo(this.journey.steps.Name);
+  }
+}
+
+module.exports = InfoPage;
