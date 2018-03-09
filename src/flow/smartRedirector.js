f060f3639ad3f1ccdbf82793fbfd7a41c8786bc1
Michael Allen <michael@michaelallen.io>
2018-03-09 12:36:04 +0000

    Stop suppressing errors from the flowControls, such as infinite loops

--- src/flow/smartRedirector.js
+++ src/flow/smartRedirector.js
@@ -1,14 +1,14 @@
 const Redirector = require('./redirector');
+const { defined } = require('../util/checks');
 
 class SmartRedirector extends Redirector {
   redirect(req, res) {
     const instance = req.journey.instance(this.nextStep);
-    try {
-      const nextStep = instance.flowControl.last();
-      res.redirect(nextStep.path);
-    } catch (error) {
+    if (!defined(instance.flowControl)) {
       throw new Error(`${instance.name} does not have a flowControl.`);
     }
+    const nextStep = instance.flowControl.last();
+    res.redirect(nextStep.path);
   }
 }
 
