0e6e5b4ed5fb230ecf7332d5c84eeea0101f6278
John O'Neill <j.oneill@kainos.com>
2018-10-18 10:29:06 +0100

    session is destroyed after render

--- test/session/destroySession.test.js
+++ test/session/destroySession.test.js
@@ -3,7 +3,10 @@ const destroySession = require('../../src/session/destroySession');
 
 describe('session/destroySession', () => {
   it('calls req.session.destroy() to destroy a session', done => {
-    const req = { session: { destroy: sinon.stub() } };
+    const req = {
+      session: { destroy: sinon.stub() },
+      on: sinon.stub().callsArg(1)
+    };
     const res = {};
     const next = () => {
       expect(req.session.destroy).calledOnce;
