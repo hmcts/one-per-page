af8aff3f8d3c02f782fabf3ed53805ce62f2afad
Mounika <mounika.ammineni@hmcts.net>
2019-01-23 13:53:11 +0000

    parsebool util added

---
+++ test/util/parseBool.test.js
@@ -0,0 +1,42 @@
+const modulePath = '../../src/util/checks';
+const parseBool = require(modulePath);
+
+const { expect } = require('./chai');
+
+describe(modulePath, () => {
+  it('should return true when given boolean true', () => {
+    expect(true, parseBool(true));
+  });
+
+  it('should return true when given string true', () => {
+    expect(true, parseBool('true'));
+  });
+
+  it('should return true when given string yes', () => {
+    expect(true, parseBool('yes'));
+  });
+
+  it('should return true when given integer 1', () => {
+    expect(true, parseBool(1));
+  });
+
+  it('should return false when given boolean false', () => {
+    expect(false, parseBool(false));
+  });
+
+  it('should return false when given string false', () => {
+    expect(false, parseBool('false'));
+  });
+
+  it('should return false when given integer 0', () => {
+    expect(false, parseBool(0));
+  });
+
+  it('should return false when given string no', () => {
+    expect(false, parseBool('no'));
+  });
+
+  it('should return false when given a random string', () => {
+    expect(false, parseBool('random string'));
+  });
+});
