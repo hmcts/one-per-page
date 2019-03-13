2fba91f7cc5095fc31f7681c697f3499b758a665
Dominic Taylor <dom.taylor7@gmail.com>
2019-03-13 14:23:00 +0000

    remove emojis

--- test/middleware/sanitizeRequestBody.test.js
+++ test/middleware/sanitizeRequestBody.test.js
@@ -2,21 +2,25 @@
 const { expect, sinon } = require('../util/chai');
 const sanitizeRequestBody = require('../../src/middleware/sanitizeRequestBody');
 const sanitizer = require('sanitizer');
+const emoji = require('node-emoji');
 
 let sanitizerSpy = {};
+let emojiSpy = {};
 let req = {};
 
 describe('sanitizeRequestBody', () => {
   beforeEach(() => {
     sanitizerSpy = sinon.spy(sanitizer, 'sanitize');
+    emojiSpy = sinon.spy(emoji, 'strip');
     req = {};
   });
 
   afterEach(() => {
     sanitizerSpy.restore();
+    emojiSpy.restore();
   });
 
-  it('runs sanitizer on each item in body', done => {
+  it('runs sanitizer and emoji stripper on each item in body', done => {
     req.body = {
       foo: 'value1',
       bar: { bar: 'value2', baz: ['array1', 'array2', 'array3'] }
@@ -24,10 +28,15 @@ describe('sanitizeRequestBody', () => {
 
     sanitizeRequestBody(req, {}, () => {
       expect(sanitizerSpy.withArgs('value1')).calledOnce;
+      expect(emojiSpy.withArgs('value1')).calledOnce;
       expect(sanitizerSpy.withArgs('value2')).calledOnce;
+      expect(emojiSpy.withArgs('value2')).calledOnce;
       expect(sanitizerSpy.withArgs('array1')).calledOnce;
+      expect(emojiSpy.withArgs('array1')).calledOnce;
       expect(sanitizerSpy.withArgs('array2')).calledOnce;
+      expect(emojiSpy.withArgs('array2')).calledOnce;
       expect(sanitizerSpy.withArgs('array3')).calledOnce;
+      expect(emojiSpy.withArgs('array3')).calledOnce;
       done();
     });
   });
@@ -38,6 +47,22 @@ describe('sanitizeRequestBody', () => {
       script2: 'some text<script>alert(document.cookie);</script>',
       script3: 'some text<script type="text/vbscript">alert(DOCUMENT.COOKIE)</script>', // eslint-disable-line max-len
       script4: 'some text\x3cscript src=http://www.example.com/malicious-code.js\x3e\x3c/script\x3e' // eslint-disable-line max-len
+    };
+
+    sanitizeRequestBody(req, {}, () => {
+      Object.keys(req.body).forEach(key => {
+        expect(req.body[key]).to.eql('some text');
+      });
+      done();
+    });
+  });
+
+  it('strips emojis from post requests', done => {
+    req.body = {
+      script1: '😆😟☹🤥🤗👿👺 some text',
+      script2: '🇧🇲🇨🇻🇬🇷🇮🇱🇨🇮🇭🇺 some text',
+      script3: '🐐🕊🐪🐳🦐🎄 some text',
+      script4: '⏺⬅🔷📢⏹↪🔈 some text'
     };
 
     sanitizeRequestBody(req, {}, () => {
